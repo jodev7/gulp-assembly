c24c6810e23565614e283f193fe3f8fd883a7824
jodev7 <joseephnv@gmail.com>
2022-04-19 16:37:22 +0500

    .

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,15 +6,27 @@ const srcFolder = './src';
 
 export const path = {
     build: {
-        files: `${buildFolder}`,
+        js: `${buildFolder}/js/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        images: `${buildFolder}/img/`,
+        files: `${buildFolder}/files/`,
+        fonts: `${buildFolder}/fonts/`,
     },
     src: {
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        js: `${srcFolder}/js/app.js`,
+        scss: `${srcFolder}/scss/style.scss`,
         html: `${srcFolder}/*.html`,
-        files: `${srcFolder}/**/*.*`,
+        files: `${srcFolder}/files/**/*.*`,
     },
     watch: {
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,ico,svg,webp}`,
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
         html: `${srcFolder}/**/*.html`,
-        files: `${srcFolder}/**/*.*`,
+        files: `${srcFolder}/files/**/*.*`,
     },
     clean: buildFolder,
     buildFolder: buildFolder,
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWeb()
+
+console.log('functions');
